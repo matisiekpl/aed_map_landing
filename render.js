bfa6b880603c0802ab7f1b4a2a8537606b486602
Mateusz Woźniak <matisiek11@gmail.com>
2023-05-20 13:15:35 +0200

    Added posts

---
+++ render.js
@@ -0,0 +1,46 @@
+const fs = require('fs');
+const axios = require('axios');
+const pug = require('pug');
+const intl = require('intl');
+const formatter = new intl.DateTimeFormat('pl', {
+    day: 'numeric',
+    month: 'long',
+    year: 'numeric'
+});
+
+async function main() {
+    let wp = await axios.get('https://wp.aedmapa.pl/wp-json/wp/v2/posts?_embed');
+    wp = wp.data;
+
+    let blog = pug.renderFile('src/pug/blog.pug');
+    let items = '';
+
+    wp.forEach(post => {
+        let content = pug.renderFile('src/pug/post.pug');
+        content = content.replace(new RegExp('{{title}}', 'g'), post['title']['rendered']);
+        content = content.replace('{{content}}', post['content']['rendered'].replace(new RegExp('<p>', 'g'), '<p class="text-xl font-extrabold leading-7 mb-10">').replace(new RegExp('<img', 'g'), '<img class="block mb-6 w-full h-112 object-cover border-3 border-indigo-900 rounded-2xl shadow-lg" '));
+        if (post['excerpt'] != null)
+            content = content.replace('{{excerpt}}', '<div class="mb-6">' + post['excerpt']['rendered'] + '</div>');
+        content = content.replace('{{date}}', formatter.format(new Date(post['date'])));
+        if (post['_embedded']['wp:featuredmedia'] != null)
+            content = content.replace('%7B%7Bimage%7D%7D', post['_embedded']['wp:featuredmedia'][0]['source_url']);
+        fs.writeFileSync('public/' + post['slug'] + '.html', content);
+        console.log('Rendered ' + post['slug'] + '.html');
+        if (post['_embedded']['wp:featuredmedia'] != null) {
+            let item = fs.readFileSync('post.html', 'utf8');
+            item = item.replace('{{title}}', post['title']['rendered']);
+            item = item.replace('{{link}}', '/' + post['slug'] + '.html');
+            item = item.replace('{{date}}', formatter.format(new Date(post['date'])));
+            if (post['excerpt'] != null)
+                item = item.replace('{{excerpt}}', post['excerpt']['rendered']);
+            item = item.replace('{{image}}', post['_embedded']['wp:featuredmedia'][0]['source_url']);
+            items += item;
+        }
+    });
+
+    blog = blog.replace('{{posts}}', items);
+    fs.writeFileSync('public/blog.html', blog);
+
+}
+
+main();
